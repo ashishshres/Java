d9242c47d2cb12e8587cbcec19712112a3d29c37
Ashish Shrestha <ashishshr27@gmail.com>
2025-02-11 17:40:32 +0545

    challenges

--- 01_Introduction/01_Challenges/Challenge1.java
+++ 01_Introduction/01_Challenges/Challenge1.java
@@ -1,6 +1,6 @@
 // Create a class to output "good morning".
-public class ChallengeOne {
-  public static void main(String[] args){
+public class Challenge1 {
+  public static void main(String[] args) {
     System.out.println("good morning");
   }
 }
