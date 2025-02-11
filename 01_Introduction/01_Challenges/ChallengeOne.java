eaa3976ca7349adb591f60106138e38bf14b53b8
Ashish Shrestha <ashishshr27@gmail.com>
2025-02-11 16:47:06 +0545

    challenges

---
+++ 01_Introduction/01_Challenges/ChallengeOne.java
@@ -0,0 +1,6 @@
+// Create a class to output "good morning".
+public class ChallengeOne {
+  public static void main(String[] args){
+    System.out.println("good morning");
+  }
+}
---
+++ 01_Introduction/01_Challenges/ChallengeTwo.java
@@ -0,0 +1,17 @@
+/*
+Show the following pattern using println:
+*
+**
+***
+****
+*****
+*/
+public class ChallengeTwo {
+  public static void main(String[] args){
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+  }
+}
