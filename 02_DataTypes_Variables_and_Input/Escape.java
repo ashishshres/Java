9010d100e710ef18d356d97f1f30609f38373b8a
Ashish Shrestha <ashishshr27@gmail.com>
2025-02-12 12:10:38 +0545

    escape sequence

---
+++ 02_DataTypes_Variables_and_Input/Escape.java
@@ -0,0 +1,11 @@
+public class Escape {
+  public static void main(String[] args) {
+    System.out.println("Hello, \'Good Morning!\'"); // Hello, 'Good Morning'
+    System.out.println("Hello,\nGood Morning!");
+    /*
+     * Hello,
+     * Good Morning!
+     */
+    System.out.println("Hello,\tGood Morning"); // Hello, Good Morning
+  }
+}
