6272073083ae693f38a79fd7a0808ba8bb656d10
Ashish Shrestha <ashishshr27@gmail.com>
2025-02-13 16:23:37 +0545

    conditional

---
+++ 03_Operators_If_Else_and_Number_System/Conditional.java
@@ -0,0 +1,41 @@
+public class Conditional {
+  public static void main(String[] args) {
+    // boolean isMale = true;
+    // String name = "Bob";
+
+    // System.out.println("Before if");
+
+    // if (isMale) {
+    // System.out.println("Mr." + name);
+    // } else {
+    // System.out.println("Mrs." + name);
+    // }
+
+    // System.out.println("After if");
+
+    // boolean isLoggedIn = true;
+    // boolean isActive = false;
+
+    // if (isLoggedIn) {
+    // System.out.println("You are logged in.");
+    // if (isActive) {
+    // System.out.println("You are currently active.");
+    // } else {
+    // System.out.println("You are not currently active.");
+    // }
+    // } else {
+    // System.out.println("You are not logged in.");
+    // }
+
+    boolean isRed = false;
+    boolean isWhite = true;
+
+    if (isRed) {
+      System.out.println("Color is red.");
+    } else if (isWhite) {
+      System.out.println("Color is white.");
+    } else {
+      System.out.println("Color is neither red nor white");
+    }
+  }
+}
