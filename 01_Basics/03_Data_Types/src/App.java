e6e88bb42479d7d04a25c6ecfd5f88d28ed38947
Ashish Shrestha <ashishshr27@gmail.com>
2025-03-08 16:37:32 +0545

    data types

---
+++ 01_Basics/03_Data_Types/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) {
+        int num = 10;
+        byte by = 123;
+        short sh = 558;
+        long l = 5431l;
+
+        float f = 3.21f;
+        double d = 3.21;
+
+        char c = 'A';
+        boolean b = true;
+
+        // Printing values
+        System.out.println("Integer: " + num); // 10
+        System.out.println("Byte: " + by); // 123
+        System.out.println("Short: " + sh); // 558
+        System.out.println("Long: " + l); // 5431l
+        System.out.println("Float: " + f); // 3.21
+        System.out.println("Double: " + d); // 3.21
+        System.out.println("Character: " + c); // A
+        System.out.println("Boolean: " + b); // true
+    }
+}
