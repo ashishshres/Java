f32e3e9bf7ebcee4019ace1b799e9cf0d1e62b0b
Ashish Shrestha <ashishshr27@gmail.com>
2025-03-08 16:30:18 +0545

    variables

---
+++ 01_Basics/02_Variables/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num = 3;
+        System.out.println(num); // 3
+
+        int first = 10;
+        int second = 20;
+        int sum = first + second;
+        System.out.println(sum); // 30
+    }
+}
