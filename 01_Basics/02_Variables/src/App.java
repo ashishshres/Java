45cb22fbc9fdb1489da73b208287928b03266fd1
Ashish Shrestha <ashishshr27@gmail.com>
2025-03-08 16:32:03 +0545

    variables

--- 01_Basics/02_Variables/src/App.java
+++ 01_Basics/02_Variables/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int num = 3;
         System.out.println(num); // 3
 
