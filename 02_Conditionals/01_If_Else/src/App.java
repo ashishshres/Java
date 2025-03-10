b35282402f5d97fa90ba9a80121f89582e4526d0
Ashish Shrestha <ashishshr27@gmail.com>
2025-03-10 15:50:52 +0545

    if and else

---
+++ 02_Conditionals/01_If_Else/src/App.java
@@ -0,0 +1,41 @@
+public class App {
+    public static void main(String[] args) {
+
+        // if
+        // if (true) {
+        // System.out.println("True");
+        // }
+
+        // if-else
+        int age = 20;
+        if (age > 18) {
+            System.out.println("Valid age"); // Valid age
+        } else {
+            System.out.println("Invalid age");
+        }
+
+        char gender = 'F';
+        if (gender == 'M') {
+            System.out.println("Male");
+        } else {
+            System.out.println("Female"); // Female
+        }
+
+        int value = 24;
+        if (value > 20 && value < 30) {
+            System.out.println("Value between 20-30"); // Value between 20-30
+        } else {
+            System.out.println("Value out of range 20-30");
+        }
+
+        // if-elseif
+        int num = 3;
+        if (num > 0) {
+            System.out.println("Number is +ve");
+        } else if (num < 0) {
+            System.out.println("Number is -ve");
+        } else {
+            System.out.println("Number is equal to zero");
+        }
+    }
+}
