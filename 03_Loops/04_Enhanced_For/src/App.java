7a77cda407d103556a8a2ff9301e4844111c4c93
Ashish Shrestha <ashishshr27@gmail.com>
2025-03-27 19:32:50 +0545

    enhanced for loop

---
+++ 03_Loops/04_Enhanced_For/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        int nums[] = { 3, 2, 1, 4, 5 };
+        for (int n : nums) {
+            System.out.println(n);
+            /*
+             * 3
+             * 2
+             * 1
+             * 4
+             * 5
+             */
+        }
+    }
+}
